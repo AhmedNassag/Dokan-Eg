f8a68f914173191dbfd05f83bcd59dfa4c99366e
Ahmed Nabil <ahmednabilpk@github.com>
2026-07-07 13:06:33 +0300

    first commit

--- database/seeders/Category/CategorySeeder.php
+++ database/seeders/Category/CategorySeeder.php
@@ -14,13 +14,13 @@ class CategorySeeder extends Seeder
         $this->call(CategoryPermissionSeeder::class);
 
         $admin    = User::where('email', 'admin@demo.com')->first();
-        $merchant = User::where('email', 'merchant@demo.com')->first();
-        $marketer = User::where('email', 'marketer@demo.com')->first();
+        // $merchant = User::where('email', 'merchant@demo.com')->first();
+        // $marketer = User::where('email', 'marketer@demo.com')->first();
 
         Category::create(['name' => 'Admin Category 1', 'description' => 'Created by admin', 'created_by' => $admin->id]);
         Category::create(['name' => 'Admin Category 2', 'description' => 'Also created by admin', 'created_by' => $admin->id]);
-        Category::create(['name' => 'Merchant Category 1', 'description' => 'Created by merchant', 'created_by' => $merchant->id]);
-        Category::create(['name' => 'Merchant Category 2', 'description' => 'Another merchant category', 'created_by' => $merchant->id]);
-        Category::create(['name' => 'Marketer Category 1', 'description' => 'Created by marketer', 'created_by' => $marketer->id]);
+        // Category::create(['name' => 'Merchant Category 1', 'description' => 'Created by merchant', 'created_by' => $merchant->id]);
+        // Category::create(['name' => 'Merchant Category 2', 'description' => 'Another merchant category', 'created_by' => $merchant->id]);
+        // Category::create(['name' => 'Marketer Category 1', 'description' => 'Created by marketer', 'created_by' => $marketer->id]);
     }
 }
