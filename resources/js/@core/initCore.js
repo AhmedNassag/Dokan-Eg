e3514fdeb5d2e91eaded5518bd8fdcab5f3883be
Ahmed Nabil <ahmednabilpk@github.com>
2026-07-09 19:54:29 +0300

    handle navbar dynamic language

--- resources/js/@core/initCore.js
+++ resources/js/@core/initCore.js
@@ -5,30 +5,20 @@ import { cookieRef, namespaceConfig } from '@layouts/stores/config'
 import { themeConfig } from '@themeConfig'
 
 const _syncAppRtl = () => {
-  const configStore = useConfigStore()
   const storedLang = cookieRef('language', null)
   const { locale } = useI18n({ useScope: 'global' })
 
-  // TODO: Handle case where i18n can't read persisted value
   if (locale.value !== storedLang.value && storedLang.value)
     locale.value = storedLang.value
 
-  // watch and change lang attribute of html on language change
+  const configStore = useConfigStore()
+  const cfgLang = themeConfig.app.i18n.langConfig?.find(l => l.i18nLang === locale.value)
+  if (cfgLang) configStore.isAppRTL = cfgLang.isRTL
+
   watch(locale, val => {
-    // Update lang attribute of html tag
     if (typeof document !== 'undefined')
       document.documentElement.setAttribute('lang', val)
-
-    // Store selected language in cookie
     storedLang.value = val
-
-    // set isAppRtl value based on selected language
-    if (themeConfig.app.i18n.langConfig && themeConfig.app.i18n.langConfig.length) {
-      themeConfig.app.i18n.langConfig.forEach(lang => {
-        if (lang.i18nLang === storedLang.value)
-          configStore.isAppRTL = lang.isRTL
-      })
-    }
   }, { immediate: true })
 }
 
