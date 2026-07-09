2d4e559cdb20565197a5dde6732b6f85d7979d1c
Ahmed Nabil <ahmednabilpk@github.com>
2026-07-09 14:25:13 +0300

    fix login for different type of user

--- app/Http/Controllers/Api/CountryController.php
+++ app/Http/Controllers/Api/CountryController.php
@@ -20,36 +20,44 @@ class CountryController extends Controller
         $this->middleware('auth:sanctum');
 
         $this->middleware('permission:list-country', ['only' => ['index']]);
-        $this->middleware('permission:store-country', ['only' => ['store']]);
         $this->middleware('permission:show-country', ['only' => ['show']]);
+        $this->middleware('permission:store-country', ['only' => ['store']]);
         $this->middleware('permission:update-country', ['only' => ['update']]);
         $this->middleware('permission:destroy-country', ['only' => ['destroy']]);
     }
+
+
 
     public function index(Request $request, CountryFilter $filter)
     {
         return $this->country->index($request, $filter);
     }
 
+
+    
     public function show($countryId)
     {
         return $this->country->show($countryId);
     }
+
+
 
     public function store(StoreRequest $request)
     {
         return $this->country->store($request);
     }
 
+
+
     public function update($countryId, UpdateRequest $request)
     {
         return $this->country->update($countryId, $request);
     }
 
+
+
     public function destroy($countryId)
     {
         return $this->country->destroy($countryId);
     }
-
-
 }
